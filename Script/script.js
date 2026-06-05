26c228a486338ee6e3304e261154d89d637d9800
sabu-cella <samuele.cella08@gmail.com>
2026-06-05 10:49:10 +0000

    Pagina Inviato integrata nella pagina Contatti

--- Script/script.js
+++ Script/script.js
@@ -1,40 +1,55 @@
-// Apre il menu media
-document.getElementById('burger').addEventListener('click', function() {
-    document.querySelector('.menu').classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function(){
+    // Apre il menu media
+    document.getElementById('burger').addEventListener('click', function() {
+        document.querySelector('.menu').classList.toggle('active');
+    });
+
+    // Non chiude il menu mobile quando si clicca su link attivi o Armi
+    document.getElementById('Armi').addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+
+    document.querySelector('.active').addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+
+    // Non chiude il menu se si clicca sul tasto menu
+    document.getElementById('burger').addEventListener('click', function(e) {
+        e.stopPropagation();
+    });
+
+    // Chiude il menu mobile
+    document.addEventListener('click', function() {
+        document.querySelector('.menu').classList.remove('active');
+    });
+
+    /* Chiude il menu quando si clicca sui link anchor
+    document.getElementById('due-mani').addEventListener('click', function() {
+        document.querySelector('.menu').classList.remove('active');
+    });
+
+    document.getElementById('lato').addEventListener('click', function() {
+        document.querySelector('.menu').classList.remove('active');
+    });
+
+    document.getElementById('sciabola').addEventListener('click', function() {
+        document.querySelector('.menu').classList.remove('active');
+    });
+
+    document.getElementById('striscia').addEventListener('click', function() {
+        document.querySelector('.menu').classList.remove('active');
+    });*/
+
+
+    //Messaggio inviato
+    document.getElementById('invia').addEventListener('click', function(){
+        document.getElementById('form').classList.toggle('hidden');
+        document.getElementById('fatto').classList.toggle('hidden');
+    });
+
+    document.getElementById('continua').addEventListener('click', function(){
+        document.getElementById('form').classList.toggle('hidden');
+        document.getElementById('fatto').classList.toggle('hidden');
+    });
 });
 
-// Non chiude il menu mobile quando si clicca su link attivi o Armi
-document.getElementById('Armi').addEventListener('click', function(e) {
-    e.stopPropagation();
-});
-
-document.querySelector('.active').addEventListener('click', function(e) {
-    e.stopPropagation();
-});
-
-// Non chiude il menu se si clicca sul tasto menu
-document.getElementById('burger').addEventListener('click', function(e) {
-    e.stopPropagation();
-});
-
-// Chiude il menu mobile
-document.addEventListener('click', function() {
-    document.querySelector('.menu').classList.remove('active');
-});
-
-// Chiude il menu quando si clicca su i link anchor
-document.getElementById('due-mani').addEventListener('click', function() {
-    document.querySelector('.menu').classList.remove('active');
-});
-
-document.getElementById('lato').addEventListener('click', function() {
-    document.querySelector('.menu').classList.remove('active');
-});
-
-document.getElementById('sciabola').addEventListener('click', function() {
-    document.querySelector('.menu').classList.remove('active');
-});
-
-document.getElementById('striscia').addEventListener('click', function() {
-    document.querySelector('.menu').classList.remove('active');
-});
